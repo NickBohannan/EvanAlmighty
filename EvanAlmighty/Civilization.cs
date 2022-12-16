6c514e623b7757b99cf4536c1ca56b9c6f663682
Nick Bohannan <nbohannan@gmail.com>
2022-12-16 13:12:18 -0600

    first push with planet and civilization classes

---
+++ EvanAlmighty/Civilization.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace EvanAlmighty
+{
+    public class Civilization
+    {
+        private string _name;
+        private string _techLevel;
+        private string _body;
+        private string _type;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string TechLevel
+        {
+            get { return _techLevel; }
+            set { _techLevel = value; }
+        }
+
+        public string Body
+        {
+            get { return _body; }
+            set { _body = value; }
+        }
+
+        public string Type
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
+
+        public Civilization(string name, string techLevel, string body, string type)
+        {
+            Name = name;
+            TechLevel = techLevel;
+            Body = body;
+            Type = type;
+        }
+    }
+}
---
+++ EvanAlmighty/Planet.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace EvanAlmighty
+{
+    public class Planet
+    {
+        private string _name;
+        private string _type;
+        private bool _isHabitable;
+        private uint _diameter;
+        private long _distance;
+        private bool _hasCivilization;
+        private Civilization _civilization;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Type
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
+
+        public bool IsHabitable
+        {
+            get { return _isHabitable; }
+            set { _isHabitable = value; }
+        }
+
+        public uint Diameter
+        {
+            get { return _diameter; }
+            set { _diameter = value; }
+        }
+
+        public long Distance
+        {
+            get { return _distance; }
+            set { _distance = value; }
+        }
+
+        public bool HasCivilization
+        {
+            get { return _hasCivilization; }
+            set { _hasCivilization = value; }
+        }
+
+        public Civilization Civilization
+        {
+            get { return _civilization; }
+            set { _civilization = value; }
+        }
+
+        public Planet(string name, string type, bool isHabitable, uint diameter, long distance, bool hasCivilization, Civilization civilization)
+        {
+            Name = name;
+            Type = type;
+            IsHabitable = isHabitable;
+            Diameter = diameter;
+            Distance = distance;
+            HasCivilization = hasCivilization;
+            Civilization = civilization;
+        }
+    }
+}
